ee8e9aa09d7299b45e4916ac8e17011b5b9dafbb
erinchc <erinchen25@gmail.com>
2023-10-18 15:55:24 +0800

    Create Functions for Numbers.py

---
+++ Functions for Numbers.py
@@ -0,0 +1,13 @@
+print(round(2.5))
+print(round(3.5))
+# x.5, the values will be rounded up if the roundup value is an even number
+# otherwise, it will be rounded down
+print(str(3)+str(3))
+print(3+3)
+
+import math #math module
+
+print(math.e)
+print(math.pi)
+print(math.floor(4.999)) #無條件捨去
+print(math.ceil(4.0001)) #無條件進位
