09df6f910767d259fa940efc19379b85b61dcbe2
erinchc <erinchen25@gmail.com>
2023-10-18 15:55:54 +0800

    Create Tuple packing & unpacking.py

---
+++ Tuple packing & unpacking.py
@@ -0,0 +1,26 @@
+c = ('Wilson',25)
+name, age = c
+print(name)
+print(age)
+# variable names have meanings
+
+a, b = (15, 100)
+print(a)
+print(b)
+
+
+x = 25
+y = 35
+
+# temp = x   # temp = 25
+# x = y      # x = 35
+# y = temp   # y = 25
+
+x, y = y, x
+# ＝ 右邊：tuple packing
+# ＝ 左邊：tuple unpacking
+
+print(x)
+print(y)
+
+
